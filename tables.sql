f27704e420e5c53cdfdb55492f14bb3976f01466
coderbai <coderbai@gmail.com>
2015-10-17 19:52:20 +0800

    Add sql file together with statements used to create  tables for the project.

---
+++ tables.sql
@@ -0,0 +1,23 @@
+# SQL statements used to create tables for the project.
+
+use zhihu;
+
+create table pages (
+    id int unsigned not null auto_increment,
+    page_id int unsigned not null,
+    title char(255) not null,
+    content text not null,
+    update_time timestamp,
+    primary key (id),
+    unique (page_id)
+);
+
+create table keywords (
+    id int unsigned not null auto_increment,
+    keyword char(31) not null,
+    page_id int unsigned not null,
+    score smallint not null,
+    primary key (id),
+    index (keyword),
+    index (page_id)
+)
