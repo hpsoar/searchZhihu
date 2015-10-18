53680802b028041b26bb9b8c7666d64216b8aa46
coderbai <coderbai@gmail.com>
2015-10-18 23:06:30 +0800

    Add prototype for search.py.

--- search.py
+++ search.py
@@ -1,1 +1,18 @@
 """Search contents on zhihu.com."""
+
+def search():
+    """Read query from command line, return first 10 search results."""
+    pass
+
+def process_query(query):
+    """Return (at most) five most distinct keywords in the query."""
+    pass
+
+def get_page_ids(keywords):
+    """Return 10 most relevant and important pages by merging and sorting
+    pages match each keyword."""
+    pass
+
+def get_return_contents(page_ids):
+    """Get digests of pages in page_ids."""
+    pass
